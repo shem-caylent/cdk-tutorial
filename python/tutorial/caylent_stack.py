ab0c42fb8c7b51ed0d45b2986d517c1cd30fd694
Shem Sedrick <shem.sedrick@caylent.com>
2023-08-04 15:43:48 -0500

    Wired up example complete

--- python/tutorial/caylent_stack.py
+++ python/tutorial/caylent_stack.py
@@ -5,6 +5,9 @@ from aws_cdk import (
     aws_s3 as s3,
     aws_sns as sns,
     aws_sqs as sqs,
+
+    aws_s3_notifications as notifications,
+    aws_sns_subscriptions as subscriptions
 )
 from constructs import Construct
 
@@ -19,3 +22,7 @@ class CaylentStack(Stack):
         topic = sns.Topic(self, "EventTopic", master_key=key)
         queue1 = sqs.Queue(self, "QueueOne", encryption_master_key=key)
         queue2 = sqs.Queue(self, "QueueTwo", encryption_master_key=key)
+
+        bucket.add_event_notifications(s3.EventType.OBJECT_CREATED_PUT, notifications.SnsDestination(topic))
+        topic.add_subscription(subscriptions.SqsSubscription(queue1))
+        topic.add_subscription(subscriptions.SqsSubscription(queue2))
--- typescript/lib/caylent-stack.ts
+++ typescript/lib/caylent-stack.ts
@@ -2,7 +2,9 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as kms from "aws-cdk-lib/aws-kms";
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as notifications from "aws-cdk-lib/aws-s3-notifications";
 import * as sns from "aws-cdk-lib/aws-sns";
+import * as subscriptions from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as sqs from "aws-cdk-lib/aws-sqs";
 
 export class CaylentStack extends cdk.Stack {
@@ -23,5 +25,9 @@ export class CaylentStack extends cdk.Stack {
     const queue2 = new sqs.Queue(this, "QueueTwo", {
       encryptionMasterKey: encryptionKey,
     });
+
+    bucket.addEventNotification(s3.EventType.OBJECT_CREATED_PUT, new notifications.SnsDestination(topic));
+    topic.addSubscription(new subscriptions.SqsSubscription(queue1))
+    topic.addSubscription(new subscriptions.SqsSubscription(queue2))
   }
 }
